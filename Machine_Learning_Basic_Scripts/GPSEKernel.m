a7dc519f637509f180cec3394d216fb648aebbba
MvHaren <maxvanharen@live.nl>
2020-05-26 13:00:35 +0200

    Added periodic kernel and made ML scripts nice

--- Machine_Learning_Basic_Scripts/GPSEKernel.m
+++ Machine_Learning_Basic_Scripts/GPSEKernel.m
@@ -3,8 +3,12 @@ function val = GPSEKernel(a,b,l)
     % b = x data direction 2
     % l = length parameter
     %% calculation
-    [D, n] = size(a);
-    [d, m] = size(b);
-    sqdist = repmat(a.^2,1,m) + repmat((b.^2)',n,1) - 2*a*b';
+    a = reshape(a, [],1 );
+    b = reshape(b, [],1 );
+%     [D, n] = size(a);
+%     [d, m] = size(b);
+%     sqdist = repmat(a.^2,1,m) + repmat((b.^2)',n,1) - 2*a*b';
+    sqdist = (repmat(a,size(b'))-repmat(b',size(a))).^2;
     val = exp(-0.5/l^2*sqdist);
+%     K(p,q) = exp(-0.5*(repmat(p',size(q))-repmat(q,size(p'))).^2)
 end
--- Machine_Learning_Basic_Scripts/GPexample.m
+++ Machine_Learning_Basic_Scripts/GPexample.m
@@ -1,50 +1,90 @@
 close all; clear all;
 
 trueF = @(x) sin(0.9*x);
+% trueF = @(x) sin(x)-cos(x);
 
-n = 50;                                     % number of test points
-N = 8;                                     % number of training points
-s = 0.00005;                                % noise variance on data
-lenghtP = 3.3;
+n =500;                                     % number of test points
+N = 10;                                     % number of training points
+s = 0.005;                                % noise variance on data
+p = 15;
+lengthP =1;
 
-xexample = linspace(-5,5,n)';               % test points
-k_ss = GPSEKernel(xexample,xexample,lenghtP);   % kernel at test points
+dist = 10;
 
 % Training data x &  y
-X = (rand(N,1)-0.5)*10;
-% y = trueF(X) + s*randn(N,1);
-y = trueF(X); % noiseless
+X = (rand(N,1)-0.5)*dist;
+y = trueF(X) + s*randn(N,1);
+% y = trueF(X); % noiseless
 
 % kernel function to our training data
-k = GPSEKernel(X,X,lenghtP); 
-L = chol(k+1e-15*eye(N),'lower');         % cholesky of kernel matrix
+k = GPSEKernel(X,X,lengthP);
+kp = PeriodicKernel(X,X,p,lengthP);
+L = chol(k+s*eye(N),'lower');         % cholesky of kernel matrix
+Lp = chol(kp+s*eye(N),'lower'); 
+
+Xtest = linspace(-dist/2,dist/2,n)';               % test points
 
 % mean for the test points
-k_s = GPSEKernel(X,xexample,lenghtP);
+k_s = GPSEKernel(X,Xtest,lengthP);
+k_sp = PeriodicKernel(X,Xtest,p,lengthP);
+
 Lk = L \ k_s;
-mu = (Lk') * (L \ y)
+Lkp = Lp \ k_sp;
+
+mu = (Lk') * (L \ y);
+mup = (Lkp') * (Lp \ y);
 
 % SD
- s2 = diag(k_ss)' - sum(Lk.^2,1);
- stdv = sqrt(s2);
- 
- % draw samples from posterior at test points
-Lss = chol(k_ss + 1e-9*eye(n)-Lk'*Lk,'lower');
-Lss_prior = chol(k_ss+1e-15*eye(n));
-f_post = mu+Lss*randn(n,N);
-f_prior = Lss_prior*randn(n,N);
+k_ss = GPSEKernel(Xtest,Xtest,lengthP);   % kernel at test points
+k_ssp = PeriodicKernel(Xtest,Xtest,p,lengthP);
+s2 = diag(k_ss)' - sum(Lk.^2,1);
+s2p = diag(k_ssp)' - sum(Lkp.^2,1);
+stdv = sqrt(s2);
+stdv2 = sqrt(s2p);
 
-
-
-figure(1)
-plot(X,y,'r*'); hold on;
-plot(xexample,trueF(xexample),'b');
-plot(xexample,mu,'r--');
-plot(xexample,mu-3*stdv','black:');
-plot(xexample,mu+3*stdv','black:');
+%% mu, true function, samples and stdv plot
+figure(1);clf;
+plot(X,y,'+','MarkerSize',15); hold on;
+plot(Xtest,trueF(Xtest),'LineWidth',1.3);
+plot(Xtest,mu,'--','LineWidth',1.3);
+plot(Xtest,mu-3*stdv','black');
+plot(Xtest,mu+3*stdv','black');
 inBetween = [(mu+3*stdv')' fliplr((mu-3*stdv')')];
-x2 = [xexample', fliplr(xexample')];
-hue = fill(x2,inBetween, [0.17 0.17 0.17],'LineStyle','none');
+x2 = [Xtest', fliplr(Xtest')];
+hue = fill(x2,inBetween, [7 7 7]/8,'LineStyle','none');
 alpha(0.3);
-legend('Generated samples','True function','Mu of fitted posterior function','mu +/- 3 sigma')
+xlabel('input, x','interpreter','Latex');
+ylabel('output, f(x)','interpreter','Latex');
+legend('Generated samples','True function','Mu of fitted posterior function','$\mu \pm 3 \sigma$','interpreter','Latex')
+figure(2)
+plot(X,y,'+','MarkerSize',15); hold on;
+plot(Xtest,trueF(Xtest),'LineWidth',1.3);
+plot(Xtest,mup,'LineWidth',1.3);
+plot(Xtest,mup-3*stdv2','black');
+plot(Xtest,mup+3*stdv2','black');
+inBetween = [(mup+3*stdv2')' fliplr((mup-3*stdv2')')];
+x2 = [Xtest', fliplr(Xtest')];
+hue = fill(x2,inBetween, [7 7 7]/8,'LineStyle','none');
+alpha(0.3);
+xlabel('input, x','interpreter','Latex');
+ylabel('output, f(x)','interpreter','Latex');
+legend('Generated samples','True function','Mu of fitted posterior function','$\mu \pm 3 \sigma$','interpreter','Latex')
 
+%% Prior calcs and plots
+Lss_prior = chol(k_ss+1e-9*eye(n),'lower');
+Lss_priorp = chol(k_ssp+1e-9*eye(n),'lower');
+f_prior = Lss_prior*randn(n,2);
+f_priorp = Lss_priorp*randn(n,2);
+figure(3)
+plot(Xtest,f_prior);
+title('Two samples from prior with SE kernel')
+xlabel('x');
+ylabel('f(x)');
+figure(4)
+plot(Xtest,f_priorp);
+title('Two samples from prior with periodic kernel')
+xlabel('x');
+ylabel('f(x)');
+%% posterior calcs and plots
+Lss_post = chol(k_ss + 1e-6*eye(n)-Lk'*Lk,'lower');
+f_post = mu+Lss_post*randn(n,N);
