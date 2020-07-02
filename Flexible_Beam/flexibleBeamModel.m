d21d0ec527785d796c95c38dff837850a0198f93
MvHaren <maxvanharen@live.nl>
2020-07-02 17:49:29 +0200

    improvements in plotting/bases

--- Flexible_Beam/flexibleBeamModel.m
+++ Flexible_Beam/flexibleBeamModel.m
@@ -15,15 +15,16 @@ HMMS = 5;                                                                   % [-
 CS=3;                                                                       % [-], shape of beam, 3 means rectangular
 
 Ix = [1 361 721];                                                           % indices on beam for Gy and Gz estimation. 1 means beginning of beam, 721 is the end
-indices = [361 10 110 220 550 660 720];                                     % indices to perform ILC with BF to estimate FF parameters
-newIndices = [60 165 455 605 690];                                          % indices to perform GP/nearest neighbour on 
+indices = [361 110 220 550 660 720];                                     % indices to perform ILC with BF to estimate FF parameters
+newIndices = [165 455 605 690];                                          % indices to perform GP/nearest neighbour on 
 N = length(indices);
 
-noPriors = 200;                                                             % number of priors for GP regression
+noPriors = 100;                                                             % number of priors for GP regression
 h = @(x) [ones(length(x(:)),1) x(:) x(:).^2 x(:).^3 x(:).^4 x(:).^5];       % basis for mean function, use prior model knowledge for this(!)
+% h = @(x) [ones(length(x(:)),1) x(:) x(:).^2];
 series = 1;                                                                 % 1=Do least squares before GP to determine mean function. 0=determine mean function using optimization in parallel with hyper parameter optimization
 
-N_trials_ILC =6;                                                            % amount of trials done in ILC
+N_trials_ILC =3;                                                            % amount of trials done in ILC
 %% UI
 methodLQ = questdlg('constructing L & Q from:', ...
     'constructing L & Q from:', ...
@@ -73,7 +74,7 @@ Gz = G{2};
 Gy = 0.5*(G{1}+G{end});
 %% ILC
 for i = 1:N
-    [thetaGrid(:,i),Gu,history(i,:)] = FlexibleBeamILCBF(indices(i),toeplitzc,indx,N_trials_ILC,1,W,P,omegaList,zeta);
+    [thetaGrid(:,i),Gu,history(i,:)] = FlexibleBeamILCBF(indices(i),toeplitzc,indx,N_trials_ILC,0,W,P,omegaList,zeta);
     close gcf;
 end
 %% GP
@@ -94,13 +95,13 @@ end
 %%
 figure
 for i = 1:N
-    p1 = plot(X(indices(i)),history(i,:).eNorm(1,end),'+','Color',c1,'Markersize',15); hold on;
-    p2 = plot(X(indices(i)),history(i,:).eNorm(1,1),'p','Color',c6,'Markersize',10); 
+    p1 = semilogy(X(indices(i)),history(i,:).eNorm(1,end),'+','Color',c1,'Markersize',15); hold on;
+    p2 = semilogy(X(indices(i)),history(i,:).eNorm(1,1),'p','Color',c6,'Markersize',10); 
 end
 for i = 1:length(newIndices)
-    p3 = plot(X(newIndices(i)),historyGP(i,:).eNorm(1,1),'s','Color',c2,'MarkerFaceColor',c2,'Markersize',10);
-    p4 = plot(X(newIndices(i)),historyBF(i,:).eNorm(1,1),'.','Color',c4,'Markersize',30);
-    p5 = plot(X(newIndices(i)),historyBF2(i,:).eNorm(1,1),'^','Color',c5,'MarkerFaceColor',c5,'Markersize',10);
+    p3 = semilogy(X(newIndices(i)),historyGP(i,:).eNorm(1,1),'s','Color',c2,'MarkerFaceColor',c2,'Markersize',10);
+    p4 = semilogy(X(newIndices(i)),historyBF(i,:).eNorm(1,1),'.','Color',c4,'Markersize',30);
+    p5 = semilogy(X(newIndices(i)),historyBF2(i,:).eNorm(1,1),'^','Color',c5,'MarkerFaceColor',c5,'Markersize',10);
 end
 
 
@@ -110,13 +111,13 @@ legend([p1 p2 p3 p4 p5],{'Training data with converged FF parameters','No feedfo
 %%
 figure
 for i = 1:N
-    p1 = plot(X(indices(i)),history(i,:).eInfNorm(1,end),'+','Color',c1,'Markersize',15); hold on;
-    p2 = plot(X(indices(i)),history(i,:).eInfNorm(1,1),'p','Color',c6,'Markersize',10);
+    p1 = semilogy(X(indices(i)),history(i,:).eInfNorm(1,end),'+','Color',c1,'Markersize',15); hold on;
+    p2 = semilogy(X(indices(i)),history(i,:).eInfNorm(1,1),'p','Color',c6,'Markersize',10);
 end
 for i = 1:length(newIndices)
-    p3 = plot(X(newIndices(i)),historyGP(i,:).eInfNorm(1,1),'s','Color',c2,'MarkerFaceColor',c2,'Markersize',10);
-    p4 = plot(X(newIndices(i)),historyBF(i,:).eInfNorm(1,1),'.','Color',c4,'Markersize',30);
-    p5 = plot(X(newIndices(i)),historyBF2(i,:).eInfNorm(1,1),'^','Color',c5,'MarkerFaceColor',c5,'Markersize',10);
+    p3 = semilogy(X(newIndices(i)),historyGP(i,:).eInfNorm(1,1),'s','Color',c2,'MarkerFaceColor',c2,'Markersize',10);
+    p4 = semilogy(X(newIndices(i)),historyBF(i,:).eInfNorm(1,1),'.','Color',c4,'Markersize',30);
+    p5 = semilogy(X(newIndices(i)),historyBF2(i,:).eInfNorm(1,1),'^','Color',c5,'MarkerFaceColor',c5,'Markersize',10);
 end
 
 xlabel('Position x on free-free beam $[m]$');
