22add02f194a2e98fb25f139eae22cfddee275c9
MvHaren <maxvanharen@live.nl>
2020-07-21 14:34:00 +0200

    Small edit

--- Machine_Learning_Basic_Scripts/GPSEKernel.m
+++ Machine_Learning_Basic_Scripts/GPSEKernel.m
@@ -3,13 +3,9 @@ function val = GPSEKernel(a,b,l)
     % b = x data direction 2
     % l = length parameter
     %% calculation
-    a = reshape(a, [],1 );
-    b = reshape(b, [],1 );
-%     [D, n] = size(a);
-%     [d, m] = size(b);
-%     sqdist = repmat(a.^2,1,m) + repmat((b.^2)',n,1) - 2*a*b';
+    a = a(:);
+    b = b(:);
+
     sqdist = (repmat(a,size(b'))-repmat(b',size(a))).^2;
-%     val = exp(-0.5/l^2*sqdist);
     val = exp(-0.5/l*sqdist);
-%     K(p,q) = exp(-0.5*(repmat(p',size(q))-repmat(q,size(p'))).^2)
 end
