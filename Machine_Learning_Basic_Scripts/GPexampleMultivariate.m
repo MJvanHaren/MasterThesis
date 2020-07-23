52f0fe08083cd2f5a5b35ae95cd59876af1b52c2
MvHaren <maxvanharen@live.nl>
2020-07-23 10:12:05 +0200

    Cleanup and plotting of multivariate GP

--- Machine_Learning_Basic_Scripts/GPexampleMultivariate.m
+++ Machine_Learning_Basic_Scripts/GPexampleMultivariate.m
@@ -1,17 +1,17 @@
 close all; clear all;clc;
 % rng(randperm(100,1),'twister')
-rng('default');
+rng('shuffle');
 trueF = @(x,z) 3*sin(0.9*x)+cos(0.5*z);
 
 
-n =250;                                     % number of test points
+n = 50;                                     % number of test points
 N = 10;                                     % number of training points (sqrt)
 
-s = 0.0005;                                % noise variance on data
+s = 0.005;                                % noise variance on data
 dist = 10;
 % Training data x &  y
-% x = sort((rand(N,1)-0.5)*dist);
-% z = sort((rand(N,1)-0.5)*dist);
+% x1 = sort((rand(N,1)-0.5)*dist);
+% x2 = sort((rand(N,1)-0.5)*dist);
 x1 = linspace(-dist/2,dist/2,N);
 x2 = linspace(-dist/2,dist/2,N);
 
@@ -34,10 +34,8 @@ end
 
 
 %% kernel function to our training data
-hyp= [5;s;1];
+hyp= [1;s;1];
 k = GPSEKernel2D(x,x,[hyp(1);hyp(1)]);
-% figure;
-% surf(x,z,k','LineStyle','none')
 Ky = hyp(3)*k+hyp(2)*eye(N^2);
 L = chol(Ky,'lower');                                       % cholesky of kernel matrix
 
@@ -54,71 +52,22 @@ for i = 1:n
 end
 
 % kernel star star
-k_ss = hyp(3)*GPSEKernel(X1,X1,hyp(1));
-
-surf(xt1,xt2,MU,'LineStyle','none')
+k_ss = hyp(3)*GPSEKernel2D(xtest,xtest,[hyp(1);hyp(1)]);
+figure(1); clf;
+subplot(1,2,1);
+surf(xt1,xt2,MU,'LineStyle','none'); hold on;
+plot3(x(:,1),x(:,2),y,'.','Color',[1 0 0],'MarkerSize',10);
+xlabel('x1 direction');
+ylabel('x2 direction');
+zlabel('Estimated function value');
 v = L\k_s;
-s2 = k_ss-v'*v;
+s2array = diag(k_ss)' - sum(Lk.^2,1);
+for i = 1:n
+   s2(:,i) = s2array(1,1+(i-1)*n:i*n);
+end
+subplot(1,2,2)
+surf(xt1,xt2,s2,'LineStyle','none');
+xlabel('x1 direction');
+ylabel('x2 direction');
+zlabel('Estimated function values variance');
 stdv = sqrt(s2);
-%% mu, true function, samples and stdv plot
-
-figure(2);clf;
-subplot(1,3,1)
-plot(X1,y,'+','MarkerSize',15); hold on;
-plot(X1,trueF(X1),'LineWidth',1.3);
-plot(X1,mu,'--','LineWidth',1.3);
-plot(X1,mu-3*stdv','black');
-plot(X1,mu+3*stdv','black');
-inBetween = [(mu+3*stdv')' fliplr((mu-3*stdv')')];
-x2 = [X1', fliplr(X1')];
-hue = fill(x2,inBetween, [7 7 7]/8,'LineStyle','none');
-alpha(0.3);
-xlabel('input, x','interpreter','Latex');
-ylabel('output, f(x)','interpreter','Latex');
-title('grid+fmincon+mean func');
-
-subplot(1,3,2)
-plot(X1,y,'+','MarkerSize',15); hold on;
-plot(X1,trueF(X1),'LineWidth',1.3);
-plot(X1,mu0,'--','LineWidth',1.3);
-plot(X1,mu0-3*stdv0','black');
-plot(X1,mu0+3*stdv0','black');
-inBetween = [(mu0+3*stdv0')' fliplr((mu0-3*stdv0')')];
-x2 = [X1', fliplr(X1')];
-hue = fill(x2,inBetween, [7 7 7]/8,'LineStyle','none');
-alpha(0.3);
-xlabel('input, x','interpreter','Latex');
-ylabel('output, f(x)','interpreter','Latex');
-title('without mean function')
-
-subplot(1,3,3)
-plot(X1,y,'+','MarkerSize',15); hold on;
-plot(X1,trueF(X1),'LineWidth',1.3);
-plot(X1,mum,'--','LineWidth',1.3);
-plot(X1,mum-3*stdvm','black');
-plot(X1,mum+3*stdvm','black');
-inBetween = [(mum+3*stdvm')' fliplr((mum-3*stdvm')')];
-x2 = [X1', fliplr(X1')];
-hue = fill(x2,inBetween, [7 7 7]/8,'LineStyle','none');
-alpha(0.3);
-title('GPML minimize w/o mean func')
-xlabel('input, x','interpreter','Latex');
-ylabel('output, f(x)','interpreter','Latex');
-legend('Generated samples','True function','Mu of fitted posterior function','$\mu \pm 3 \sigma$','interpreter','Latex')
-
-%% Prior calcs and plots
-% Lss_prior = chol(k_ss+1e-7*eye(n),'lower');
-% f_prior = Lss_prior*randn(n,2);
-% figure(3)
-% plot(Xtest,f_prior);
-% title('Two samples from prior with SE kernel')
-% xlabel('x');
-% ylabel('f(x)');
-%% posterior calcs and plots
-% Lss_post = chol(k_ss + 1e-7*eye(n)-Lk'*Lk,'lower');
-% f_post = mu+Lss_post*randn(n,2);
-% figure(4);
-% plot(Xtest,f_post);
-% title('Two samples from posterior with SE kernel')
-% xlabel('x');
-% ylabel('f(x)');
